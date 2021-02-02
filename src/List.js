8420132ff138203405cbfe26d72502b9347c4d5d
shimmy1202 <shimmy.pub@gail.com>
2021-02-03 02:31:35 +0900

    04class

--- src/List.js
+++ src/List.js
@@ -1,11 +1,23 @@
 import React from "react"
+
+const LANGUAGE = [
+  'JavaScript',
+  'C++',
+  'Ruby',
+  'Java',
+  'PHP',
+  'Go'
+];
+
 export class List extends React.Component {
   render() {
-    const { title } = this.props
     return (
       <div>
-        <h4>{title}</h4>
-        <div>リストです</div>
+        {
+          LANGUAGE.map((lang, index) => {
+            return <div key={index}>{lang}</div>
+          })
+        }
       </div>
     );
   }
