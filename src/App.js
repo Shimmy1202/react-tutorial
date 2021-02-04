e56c8081bb740de284f5ff22a9311d14f8dcbeeb
shimmy1202 <shimmy.pub@gail.com>
2021-02-05 05:57:04 +0900

    06

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { List } from "./List";
 import { Form } from "./Form";
-import { LANGUAGES } from "./languages";
+import { getLanguages } from "./languages";
 
 function App() {
+  // useState() の第一引数は初期値
   const [tab, setTab] = useState("list");
-  const [langs, setLangs] = useState(LANGUAGES);
+  const [langs, setLangs] = useState([]);
 
+  // useEffect() は、マウンティング時とアップデート時に実行される
+  // ただし、第二引数に []：空の配列 を入れるとマウンティング時のみに実行される
+  // また、第二引数に特定の変数を入れると、マウンティング時に加えて配列の中身が変化した際にも実行される。
+  useEffect(() => {
+    fetchLanguages();
+  }, [langs, tab]);
+
+  //
+  const fetchLanguages = async () => {
+    const languages = await getLanguages();
+    setLangs(languages);
+  };
   const addLang = (lang) => {
     setLangs([...langs, lang]);
     setTab("list");
--- src/languages.js
+++ src/languages.js
@@ -1,1 +1,12 @@
 export const LANGUAGES = ["JavaScript", "C++", "Ruby", "Java", "PHP", "Go"];
+
+export const getLanguages = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(LANGUAGES);
+    }, 1000);
+  });
+};
+
+// Promiseを使うのは関数の中で const など値を定義することなく return するときに使うといい
+// それ以外は async/await で書くのが良さげ
